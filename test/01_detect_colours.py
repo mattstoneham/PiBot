bd52eefb90c8fedf8f9780a89c151ecb62308a98
mattstoneham <mattstoneham@gmail.com>
2018-11-01 15:08:14 +0000

    wip

--- test/01_detect_colours.py
+++ test/01_detect_colours.py
@@ -12,9 +12,9 @@ def main():
     myColourSensor = ColourSensor()
     while True:
         rgbdict = myColourSensor.get_rgb_values()
-
+        print('Raw RGB values from sensor:')
         for key, value in rgbdict.items():
-            print(key, value)
+            print('\t'+key, value)
         print('\n')
 
         total = rgbdict['red'] + rgbdict['green'] + rgbdict['blue']
@@ -24,23 +24,23 @@ def main():
         if total < minimum:
             print('Auto calibrated min intensity')
             minimum = total
-            percentage = (100 / (maximum-minimum)) * (total - minimum)
-            print("Light percent: {0}".format(percentage))
+        percentage = (100 / (maximum-minimum)) * (total - minimum)
+        print("Light percent: {0}".format(percentage))
 
         if rgbdict['green']<3000 and rgbdict['blue']<4200 and rgbdict['red']>8000:
-            print("red")
+            print("--RED--")
             temp=1
         elif rgbdict['red']<2200 and  rgbdict['blue']<2800 and rgbdict['green']>2900:
-            print("green")
+            print("--GREEN--")
             temp=1
         elif rgbdict['green']<3700 and rgbdict['red']<2500 and rgbdict['blue']>79000 and percentage >20:
-            print("blue")
+            print("--BLUE--")
             temp=1
         elif rgbdict['red']>10000 and rgbdict['green']>10000 and rgbdict['blue']>10000:
-            print("white")
+            print("--WHITE--")
             temp=1
         elif rgbdict['red']<2000 and rgbdict['green']<2000 and rgbdict['blue']<2000:
-            print("black")
+            print("--BLACK--")
 
         time.sleep(1)
 
