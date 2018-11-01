1c8253e22e5f24f6021e299379654865fae0aa80
mattstoneham <mattstoneham@gmail.com>
2018-11-01 12:53:20 +0000

    wip

--- test/01_detect_colours.py
+++ test/01_detect_colours.py
@@ -1,9 +1,18 @@
 __author__ = 'Matt'
 
 from lib.colour_sensor import ColourSensor as cs
+import time
 
-rbgdict = cs.get_rgb_values()
+def main():
+    while True:
+        rbgdict = cs.get_rgb_values()
 
-for key, value in rbgdict.items():
-    print(key, value)
+        for key, value in rbgdict.items():
+            print(key, value)
+
+        time.sleep(1)
+
+if __name__ == 'main':
+    main()
+
 
