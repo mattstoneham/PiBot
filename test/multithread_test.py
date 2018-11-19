30662c7d099f75bd5c1ec86a1b17547a32417e58
mattstoneham <mattstoneham@gmail.com>
2018-11-19 23:20:20 +0000

    wip, and added multithread test

--- test/multithread_test.py
+++ test/multithread_test.py
@@ -19,46 +19,38 @@ class MyClass(object):
     RGBsensor = {}
 
     class SensorThread(threading.Thread):
-        def __init__(self, threadID, name):
+        def __init__(self, threadID, name, updatefreq=0.5):
             threading.Thread.__init__(self)
             self.threadID = threadID
             self.name = name
-
+            self.updatefreq = updatefreq
 
         def run(self):
             print("Starting " + self.name)
-            #MyClass().test_getsensorvalue(MyClass)
-            MyClass().getsensorvalue(MyClass)
+            MyClass().getsensorvalue(MyClass, self.updatefreq)
             print("Exiting " + self.name)
 
         def exit(self):
             MyClass.exitFlag = 1
 
-    def test_getsensorvalue(self, newself):
-        while not newself.exitFlag:
-            time.sleep(0.0001)
-            newself.value += 1
-            if newself.value > 9999:
-                newself.exitFlag = 1
-            #print('self.value: {0}'.format(newself.value))
 
-    def getsensorvalue(self, newself):
+
+    def getsensorvalue(self, newself, updatefreq):
         myColourSensor = ColourSensor()
         while not newself.exitFlag:
-            time.sleep(0.0001)
+            time.sleep(updatefreq)
             newself.RGBsensor = myColourSensor.get_rgb_values()
 
 
     def run(self):
         # Create new threads
-        self.thread1 = self.SensorThread('MySensorThread', 1)
+        self.thread1 = self.SensorThread('MySensorThread', 1, 0.5)
         print('thread1 = {0}'.format(self.thread1))
         self.thread1.start()
 
 
         while not self.exitFlag:
-            #print('Hello there self.value of {0}'.format(self.value))
-            print('RGB {0}'.format(self.RGBsensor))
+            print('\nRGB {0}'.format(self.RGBsensor))
 
 
 
